6066296fe745ac592766fb642c0d8aa45629bf16
Natalia Ramirez Parra <jnataliaramirez@outlook.com>
2023-12-26 20:05:02 +0100

    Do CheckBox component

---
+++ src/components/CheckBox/index.tsx
@@ -0,0 +1,22 @@
+import './styles.scss';
+
+interface Props {
+  htmlFor: string;
+  value: string;
+  name: string;
+  children: React.ReactNode;
+}
+
+export const CheckBox: React.FC<Props> = ({
+  htmlFor,
+  value,
+  name,
+  children,
+}) => {
+  return (
+    <label className="radio-box" htmlFor={htmlFor}>
+      {children}
+      <input id={htmlFor} type="checkbox" value={value} name={name} />
+    </label>
+  );
+};
