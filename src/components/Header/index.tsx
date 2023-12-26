6672ea725791802889c71e9b6ead8cd945da2099
Natalia Ramirez Parra <jnataliaramirez@outlook.com>
2023-12-26 16:32:57 +0100

    Add styles to header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,9 +2,11 @@ import './styles.scss';
 
 export const Header = () => {
   return (
-    <header className='header'>
-      <h1>
-        Oferta del mes <br /> Ahorra un 50%
+    <header className="header">
+      <h1 className="header__title">
+        Oferta del mes 
+        <span className="header__title--br"></span>
+        <span className="header__title--subtitle">Ahorra un 50%</span>
       </h1>
     </header>
   );
