2fdb98ca67c639e7efc90b82a610f9587befc204
Natalia Ramirez Parra <jnataliaramirez@outlook.com>
2023-12-27 13:00:06 +0100

    Do StepEnd section

--- src/sections/StepEnd/index.tsx
+++ src/sections/StepEnd/index.tsx
@@ -1,6 +1,14 @@
+import { InputTel } from '../../components/InputTel';
+import { SubmitButton } from '../../components/SubmitButton';
+import { Subtitle } from '../../components/Subtitle';
+import './styles.scss';
 
 export const StepEnd = () => {
+
   return (
-    <div>index</div>
-  )
-}
+    <section className="stepEnd">
+      <Subtitle>Guarda tus respuestas</Subtitle>
+      <InputTel button={<SubmitButton />} />
+    </section>
+  );
+};
